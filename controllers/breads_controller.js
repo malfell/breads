255e6b8cf69e5e9f2a55fe7911bfd35088f95c2b
malfell <malfellows13@gmail.com>
2022-06-12 11:54:26 -0500

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -26,6 +26,18 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+//Edit must be ABOVE show route because of the parameter thing
+//the show route would override if it's first
+//more specific on top, less specific on bottom
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
@@ -61,6 +73,16 @@ breads.delete('/:indexArray', (req, res) => {
   res.status(303).redirect('/breads')
 })
 
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
 
 
 module.exports = breads
--- views/show.jsx
+++ views/show.jsx
@@ -18,10 +18,13 @@ function Show ({bread, index}) {
             have gluten.
         </p>
         <img src={bread.image} alt={bread.name} />
-        <li><a href="/breads">Go home</a></li>
+        <li>
+          <a href="/breads">Go home</a>
+        </li>
+        {/* EDIT BUTTON */}
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 
         {/* Delete Form */}
-        
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
           <input type='submit' value="DELETE"/>
         </form>
